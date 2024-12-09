d258a9f68949039301fd83491c7c2bf556ed776d
Khadija <1011hadija@gmail.com>
2024-12-09 08:20:39 -0800

    Add files via upload

---
+++ object 1/script.js
@@ -0,0 +1,144 @@
+//creation of an object;
+const person = {
+    firstName: " Ayesha", lastName: "Sardar", age: 22, eyeColor: " brown"
+}
+// data of the object that is displayed;
+document.getElementById('demon').innerHTML = person.firstName
+    + " " + person.lastName + " " + "is" + " " + person.age + " " + "years old."
+
+
+//2nd object;
+const car = {
+    modelNumber: 402,
+    carNumber: 520,
+    carColor: "black",
+}
+//data of 2nd object;
+document.getElementById('demo').innerHTML = "My cars model number is" + " " +
+    car.modelNumber + " " + " and its car no is" + " " + car.carNumber + " " +
+    "while its colour is" + " " + car.carColor
+
+
+//3rd object;
+const flower = {
+    flowerColor: "pink",
+    flowerType: "rose",
+    flowerPetals: 20
+}
+document.getElementById('flower').innerHTML = "I have a" + " " + flower.flowerColor
+    + " " + flower.flowerType + " " + " and it have" + " " + flower.flowerPetals
+    + " " + " flower petals."
+
+    //one;
+    ;
+const one = {
+    appType: "YouTube",
+    appColor: "Red",
+    appDownloads: 10,
+}
+document.getElementById('one').innerHTML = one.appType + " " + "is " + one.appColor
+    + " " + "in Color and it have" + " " + one.appDownloads + " " + "downloads in it currently."
+//two;
+const two = {
+    siteType: "Search Engine",
+    siteColor: "Orange",
+    siteDownloads: 0
+}
+document.getElementById('two').innerHTML = "Google is a type of"
+    + " " + two.siteType + " " + "and its color is" + " " + two.siteColor + " " +
+    "while its downloads are" + " " + two.siteDownloads;
+//three;
+const three = {
+    dramaName: "Till the end of the Moon",
+    dramaType: "Fantasy",
+    dramaEpisodes: 65,
+}
+document.getElementById('three').innerHTML = three.dramaName + " " + "is a" + " " +
+    three.dramaType + " " + "type of drama and it has"
+    + " " + three.dramaEpisodes + " " + "Episodes."
+//four;
+const four = {
+    dramaName: "Arsenal Military Academy",
+    dramaType: "Action",
+    dramaEpisodes: 21,
+}
+document.getElementById('four').innerHTML = four.dramaName + " " + "is an" + " " +
+    four.dramaType + " " + "type of drama and it has"
+    + " " + four.dramaEpisodes + " " + "Episodes."
+//five
+const five = {
+    dramaName: "Khai",
+    dramaType: "Action",
+    dramaEpisodes: 45,
+}
+document.getElementById('five').innerHTML = five.dramaName + " " + "is a" + " " +
+    five.dramaType + " " + "type of drama and it has"
+    + " " + five.dramaEpisodes + " " + "Episodes."
+
+//six
+const six = {
+    flowerColor: "Black",
+    flowerType: "Rose",
+    flowerPetals: 1000
+}
+document.getElementById('six').innerHTML = "I have a" + " " + six.flowerColor
+    + " " + six.flowerType + " " + " and it have" + " " + six.flowerPetals
+    + " " + " flower petals so it looks really Aesthetic."
+//seven
+const seven = {
+    budColor: "Blue",
+    budType: "Rose",
+    budPetals: 50
+}
+document.getElementById('seven').innerHTML = "I have a" + " " + seven.budColor
+    + " " + seven.budType + " " + " and it have" + " " + seven.budPetals
+    + " " + " flower petals."
+
+
+//eight
+const eight = {
+    phoneType: "I-phone",
+    phoneColor: "White",
+    phonePrice: 20000
+}
+document.getElementById('eight').innerHTML = "My brother owns an" + " " +
+    eight.phoneType + " " + "which is" + " " + eight.phoneColor + " " + "in Color and its cost is around" +
+    " " + eight.phonePrice + " " + "."
+//nine
+const nine = {
+    phoneType: "Samsung",
+    phoneColor: "Pale Violet Red",
+    phonePrice: 30000
+}
+document.getElementById('nine').innerHTML = "I have a" + " " +
+    nine.phoneType + " " + "which is" + " " + nine.phoneColor + " " + "in Color and its cost is around" +
+    " " + nine.phonePrice + " " + "."
+//ten
+const ten = {
+    phoneType: "Hua Wei",
+    phoneColor: "Black",
+    phonePrice: 10000
+}
+document.getElementById('ten').innerHTML = "My sister has a" + " " +
+    ten.phoneType + " " + "which is" + " " + ten.phoneColor + " " + "in Color and its cost is around" +
+    " " + ten.phonePrice + " " + "."
+//eleven
+const eleven = {
+    quiltType: "Soft",
+    quiltColor: "Purple",
+    quiltPrice: 1000
+}
+document.getElementById('eleven').innerHTML = "I have a" + " " +
+    eleven.quiltType + " " + "Quilt which is" + " " + eleven.quiltColor + " " + "in Color and its price is around" +
+    " " + eleven.quiltPrice + " " + "."
+//twelve
+const twelve = {
+
+    quiltType: "Soft",
+    quiltColor: "Blue",
+    quiltPrice: 2000
+}
+document.getElementById('twelve').innerHTML = "My brother has a" + " " +
+    twelve.quiltType + " " + "Quilt which is" + " " + twelve.quiltColor + " " + "in Color and its price is around" +
+    " " + twelve.quiltPrice + " " + "."
+
